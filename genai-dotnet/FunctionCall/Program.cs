4c6c255e7b4e765349321171274999ac1db9166d
ROSHAN <roshan.bobade@outlook.com>
2026-02-11 15:56:00 +0530

    function calling

---
+++ genai-dotnet/FunctionCall/Program.cs
@@ -0,0 +1,54 @@
+﻿// get Credentials from user secrets
+
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+
+IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var credential = new ApiKeyCredential(config["GitHubModels:Token"] ?? throw new InvalidCastException("Invalid credentials"));
+
+var option = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.github.ai/inference")
+};
+
+// create a chat client
+
+IChatClient client = new ChatClientBuilder(new OpenAIClient(credential, option).GetChatClient("openai/gpt-4o-mini").AsIChatClient())
+                        .UseFunctionInvocation()
+                        .Build();
+
+var chatOptions = new ChatOptions
+{
+    Tools = [AIFunctionFactory.Create((string location, string unit) =>
+    {
+        // Here you would call a weather API to get the weather for the location
+        var temperature = Random.Shared.Next(5, 20);
+        var conditions = Random.Shared.Next(0, 1) == 0 ? "sunny" : "rainy";
+
+        return $"The weather is {temperature} degrees C and {conditions}.";
+    },
+    "get_current_weather",
+    "Get the current weather in a given location")]
+};
+
+List<ChatMessage> chatHistory = [new(ChatRole.System, """
+    You are a hiking enthusiast who helps people discover fun hikes in their area. 
+    You are upbeat and friendly.
+    """)];
+
+// Weather conversation relevant to the registered function.
+chatHistory.Add(new(ChatRole.User, """
+    I live in Pune and I'm looking for a moderate intensity hike. 
+    What's the current weather like? 
+    """));
+
+Console.WriteLine($"{chatHistory.Last().Role} >>> {chatHistory.Last()}");
+
+ChatResponse response = await client.GetResponseAsync(chatHistory, chatOptions);
+
+chatHistory.Add(new(ChatRole.Assistant, response.Text));
+
+Console.WriteLine($"{chatHistory.Last().Role} >>> {chatHistory.Last()}");
