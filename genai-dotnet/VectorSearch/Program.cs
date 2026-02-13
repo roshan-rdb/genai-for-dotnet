dcd4e15da2ebce2eccd91f1fb16bfb16492bc69a
ROSHAN <roshan.bobade@outlook.com>
2026-02-13 12:45:35 +0530

    Vector Search

---
+++ genai-dotnet/VectorSearch/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Connectors.InMemory;
+using OpenAI;
+using System.ClientModel;
+using VectorSearch;
+
+IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var credential = new ApiKeyCredential(config["GitHubModels:Token"] ?? throw new InvalidCastException("Invalid credentials"));
+
+var option = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.github.ai/inference")
+};
+
+IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator =
+    new OpenAIClient(credential, option)
+    .GetEmbeddingClient("openai/text-embedding-3-small")
+    .AsIEmbeddingGenerator();
+
+var vectorStore = new InMemoryVectorStore();
+var moviesStore = vectorStore.GetCollection<int, Movie>("movies");
+await moviesStore.EnsureCollectionExistsAsync();
+
+foreach (var movie in MovieData.Movies)
+{
+    movie.Vector = await embeddingGenerator.GenerateVectorAsync(movie.Description);
+    await moviesStore.UpsertAsync(movie);
+}
+
+//var query = "I want to see familly friendly movies";
+var query = "I want to see science fiction movies";
+var queryEmbedding = await embeddingGenerator.GenerateVectorAsync(query);
+
+var searchResults = moviesStore.SearchAsync(queryEmbedding, top: 2);
+
+await foreach( var result in searchResults)
+{
+    Console.WriteLine($"Title: {result.Record.Title}");
+    Console.WriteLine($"Description : {result.Record.Description}");
+    Console.WriteLine($"Similarity Score: {result.Score}");
+    Console.WriteLine();
+}
---
+++ genai-dotnet/VectorSearch_Ollama/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.SemanticKernel.Connectors.InMemory;
+using OllamaSharp;
+using VectorSearch_Ollama;
+
+
+IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator =
+    new OllamaApiClient(new Uri("http://localhost:11434"),"all-minilm");
+
+var vectorStore = new InMemoryVectorStore();
+var moviesStore = vectorStore.GetCollection<int, Movie>("movies");
+await moviesStore.EnsureCollectionExistsAsync();
+
+foreach (var movie in MovieData.Movies)
+{
+    movie.Vector = await embeddingGenerator.GenerateVectorAsync(movie.Description);
+    await moviesStore.UpsertAsync(movie);
+}
+
+var query = "I want to see science fiction movies";
+var queryEmbedding = await embeddingGenerator.GenerateVectorAsync(query);
+
+var searchResults = moviesStore.SearchAsync(queryEmbedding, top: 2);
+
+await foreach (var result in searchResults)
+{
+    Console.WriteLine($"Title: {result.Record.Title}");
+    Console.WriteLine($"Description : {result.Record.Description}");
+    Console.WriteLine($"Similarity Score: {result.Score}");
+    Console.WriteLine();
+}
