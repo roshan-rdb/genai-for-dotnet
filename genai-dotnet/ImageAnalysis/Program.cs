275c04c105dacea3b7111fb39711c1f2a4a2246f
ROSHAN <roshan.bobade@outlook.com>
2026-02-14 15:22:24 +0530

    image analysis

---
+++ genai-dotnet/ImageAnalysis/Program.cs
@@ -0,0 +1,45 @@
+﻿// get Credentials from user secrets
+
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using OpenAI;
+using System.ClientModel;
+
+IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+var credential = new ApiKeyCredential(config["GitHubModels:Token"] ?? throw new InvalidCastException("Invalid credentials"));
+
+var option = new OpenAIClientOptions()
+{
+    Endpoint = new Uri("https://models.github.ai/inference")
+};
+
+IChatClient client = new OpenAIClient(credential, option).GetChatClient("openai/gpt-4o-mini").AsIChatClient();
+
+// user prompts
+var promptDescribe = "Describe the image";
+var promptAnalyze = "How many red cars are in the picture? and what other car colors are there?";
+
+// prompts
+string systemPrompt = "You are a useful assistant that describes images using a direct style.";
+var userPrompt = promptDescribe; //promptAnalyze;
+
+List<ChatMessage> messages =
+[
+    new ChatMessage(ChatRole.System, systemPrompt),
+    new ChatMessage(ChatRole.User, userPrompt),
+];
+
+// read the image bytes, create a new image content part and add it to the messages
+var imageFileName = "cars.png";
+string image = Path.Combine(Directory.GetCurrentDirectory(), "images", imageFileName);
+
+AIContent aic = new DataContent(File.ReadAllBytes(image), "image/png");
+var message = new ChatMessage(ChatRole.User, [aic]);
+messages.Add(message);
+
+// send the messages to the assistant
+var response = await client.GetResponseAsync(messages);
+Console.WriteLine($"Prompt: {userPrompt}");
+Console.WriteLine($"Image: {imageFileName}");
+Console.WriteLine($"Response: {response.Messages[0]}");
